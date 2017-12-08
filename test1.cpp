bb58e8c4025faa3be4a5f4b0ff8b32a3e268de58
moguchev <32189038+moguchev@users.noreply.github.com>
2017-12-08 11:29:22 +0300

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,13 +1,64 @@
 #include <iostream>
 
+bool queen(std::pair<char, int> black, std::pair<char, int> whitemove)
+{
+    if ((whitemove.first == black.first) || (whitemove.second == black.second) ||
+        (std::abs(whitemove.first - black.first) == std::abs(whitemove.second - black.second)))
+        return true;
+    else
+        return false;
+}
+bool elephant(std::pair<char, int> black, std::pair<char, int> whitemove)
+{
+    if (std::abs(whitemove.first - black.first) == std::abs(whitemove.second - black.second))
+        return true;
+    else
+        return false;
+}
+bool rook(std::pair<char, int> black, std::pair<char, int> whitemove)
+{
+    if ((whitemove.first == black.first) || (whitemove.second == black.second))
+        return true;
+    else
+        return false;
+}
+bool paw(std::pair<char, int> black, std::pair<char, int> whitemove)
+{
+    if ((whitemove.first == black.first) &&
+        ((whitemove.second == black.second) || (whitemove.second == black.second - 1)))
+        return false;
+    else if (((whitemove.first + 1 == black.first) && (whitemove.second - 1 == black.second)) ||
+             ((whitemove.first - 1 == black.first) && (whitemove.second + 1 == black.second)))
+        return true;
+    else if ((whitemove.first == 2) && ((whitemove.first == black.first) &&
+                                        (whitemove.second + 2 == black.second)) ||
+             (whitemove.first == black.first) && (whitemove.second + 1 == black.second))
+        return true;
+    else
+        return false;
+}
+bool horse(std::pair<char, int> black, std::pair<char, int> whitemove)
+{
+    if (((whitemove.first == black.first + 2) ||
+         (whitemove.first == black.first - 2) &&
+         (whitemove.second == black.second + 1) ||
+         (whitemove.second == black.second - 1)) ||
+        ((whitemove.first == black.first + 1) ||
+         (whitemove.first == black.first - 1) &&
+         (whitemove.second == black.second + 2) ||
+         (whitemove.second == black.second - 2)))
+        return true;
+    else
+        return false;
+}
 int main()
 {
-    bool flag;
+
     std::pair<char, int> black;
     black.first = 'g';
     black.second = 3;
     std::cout << "What chess piece?" << std::endl;
-    char namefigure = 0;
+    char namefigure;
     std::cin >> namefigure;
     std::cout << "initial coordinate" << std::endl;
     std::pair<char, int> whitemove;
@@ -15,60 +66,45 @@ int main()
     //////////////check correct coordinate
     if ((whitemove.first > 'h') || (whitemove.second > 8))
     {
-        while (flag == false)
+        bool flag;
+        do
         {
             std::cout << "initial coordinate" << std::endl;
             std::cin >> whitemove.first >> whitemove.second;
-            if ((whitemove.first <= 'h') && (whitemove.second <= 8))
+            if (whitemove.first <= 'h' && whitemove.second <= 8)
                 flag = true;
             else
                 flag = false;
-        }
+        } while (!flag);
     }
     switch (namefigure)
     {
         case 'Q':
-            if ((whitemove.first == black.first) || (whitemove.second == black.second) ||
-                (std::abs(whitemove.first - black.first) == std::abs(whitemove.second - black.second)))
+            if (queen(black,whitemove))
                 std::cout << "Queen true" << std::endl;
             else
                 std::cout << "Queen false!" << std::endl;
             break;
         case 'B':
-            if (std::abs(whitemove.first - black.first) == std::abs(whitemove.second - black.second))
+            if (elephant(black,whitemove))
                 std::cout << "Elephant true" << std::endl;
             else
                 std::cout << "Elephant false" << std::endl;
             break;
         case 'R':
-            if ((whitemove.first == black.first) || (whitemove.second == black.second))
+            if (rook(black,whitemove))
                 std::cout << "Rook true" << std::endl;
             else
                 std::cout << "Rook false!" << std::endl;
             break;
         case 'p':
-            if ((whitemove.first == black.first) &&
-                ((whitemove.second == black.second) || (whitemove.second == black.second - 1)))
-                std::cout << "Paw false!" << std::endl;
-            else if (((whitemove.first + 1 == black.first) && (whitemove.second - 1 == black.second)) ||
-                       ((whitemove.first - 1 == black.first) && (whitemove.second + 1 == black.second)))
-                std::cout << "Paw true" << std::endl;
-            else if ((whitemove.first == 2) && ((whitemove.first == black.first) &&
-                                                  (whitemove.second + 2 == black.second)) ||
-                       (whitemove.first == black.first) && (whitemove.second + 1 == black.second))
+            if (paw(black,whitemove))
                 std::cout << "Paw true " << std::endl;
             else
                 std::cout << "Paw false!" << std::endl;
             break;
         case 'N':
-            if (((whitemove.first == black.first + 2) ||
-                 (whitemove.first == black.first - 2) &&
-                 (whitemove.second == black.second + 1) ||
-                 (whitemove.second == black.second - 1)) ||
-                ((whitemove.first == black.first + 1) ||
-                 (whitemove.first == black.first - 1) &&
-                 (whitemove.second == black.second + 2) ||
-                 (whitemove.second == black.second - 2)))
+            if (horse(black,whitemove))
                 std::cout << "Horse true";
             else
                 std::cout << "Horse false!";
